304b8ae17c18604ca518dc933988acaaee163d84
dmc1778 <nimashiri2012@gmail.com>
2023-06-16 10:30:42 -0400

    useful prompts added.

--- core/runllm.py
+++ core/runllm.py
@@ -64,28 +64,71 @@ def create_prompt(item, model='tf'):
         sample_code = item["Sample Code"]
         bug_fix = item["Bug fix"]
 
-        _prompt = f"""
+        _prompt_steps = f"""
                 Given following pieces of information:\
                     Title: {Title} \
                     Bug description: {bug_description} \
                     Minimum reproduceable example: {sample_code} \
                     Bug fix: {bug_fix}
                 
-                Generate a malformed input generation rule for the mentioned API in the issue. The rule should be usable for fuzzing. \
-                Generate the rule based on the malicious inputs explained in the title and bug description.
-                Do not suggest any fix.\
-                Do not explain what is the weakness in the backend.\
-                Don't generate any example.
+                Your task is to generate a malformed input generation rule. The rule should be usable for fuzzing. \
+                To better understand how to generate the rule, please consider the following steps:\
+                    
+                1 - Figure out which TensorFlow API is mentioned in the ```Title``` or ```Bug description```\
+                2 - Understand input specification for the API
+                3 - Understand what input is causing bug, i.e., it should be explain in the ```Title``` or ```Bug description```\
+                4 - Understand the impact of the bug explained in the ```Bug description```\
+                5 - Understand the minimum reproduceable example that cause the bug \
+                    
+                Based on above steps, generate the rule based on the malicious inputs explained in the title and bug description. \
+                
+                Perform the rule generation based on the following constraints:
+                1 - Do not suggest any fix.\
+                2 - Do not explain what is the weakness in the backend.\
+                3 - Do not generate any example. \
+                4 - Do not explain the input specification.\
+                5 - Concisely generate the rule for fuzzing.
                 """
-    return _prompt
+
+        _prompt_general = f"""
+                Given following pieces of information:\
+                    Title: {Title} \
+                    Bug description: {bug_description} \
+                    Minimum reproduceable example: {sample_code} \
+                    Bug fix: {bug_fix}
+                
+                Your task is to generate a malformed input generation rule. The rule should be usable for fuzzing. \
+                To better understand how to generate the rule, please consider the following steps:\
+                    
+                1 - Figure out which TensorFlow API is mentioned in the ```Title``` or ```Bug description```\
+                2 - Understand input specification for the API
+                3 - Understand what input is causing bug, i.e., it should be explain in the ```Title``` or ```Bug description```\
+                4 - Understand the impact of the bug explained in the ```Bug description```\
+                5 - Understand the minimum reproduceable example that cause the bug \
+                    
+                Based on above steps, generate the rule based on the malicious inputs explained in the title and bug description. \
+                ```Your task is to generate a general rule that is applicable for other APIs as well```
+                ```For example, if the bug description says the bug is due to feeding very large integer value for \
+                    the paramer ```dim```, this means that we can use ```A Very Large Integer Value``` as a general\
+                        rule for all APIs that have integer argument. \
+                
+                
+                Perform the rule generation based on the following constraints:
+                1 - Do not suggest any fix.\
+                2 - Do not explain what is the weakness in the backend.\
+                3 - Do not generate any example. \
+                4 - Do not explain the input specification.\
+                5 - Concisely generate the rule for fuzzing.
+                """
+    return _prompt_general
 
 
 def run():
 
     lib_name = 'tf'
-    model_name = 'gpt-3.5-turbo-0301'
+    model_name = 'gpt-3.5-turbo'
 
-    rules_path = f"rulebase/{lib_name}_rules_textual.json"
+    rules_path = f"rulebase/{lib_name}_rules_general.json"
 
     with open(f'data/{lib_name}_bug_data.json') as json_file:
         data = json.load(json_file)
